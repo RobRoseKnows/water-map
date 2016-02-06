104e55ad4d2ac70f17c5e0272b172208a8a39873
Robert <robert.craynon@gmail.com>
2016-02-06 16:25:39 -0500

    Added WaterPin; Dynamo INterfacing

---
+++ app/src/main/java/io/robrose/hop/watermap/aws/util/WaterPin.java
@@ -0,0 +1,22 @@
+package io.robrose.hop.watermap.aws.util;
+
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+
+import java.util.Map;
+
+/**
+ * Created by Robert C. on 2/6/2016.
+ */
+public class WaterPin {
+
+        public String violCatCode;
+        public String contamCode;
+        public String violCode;
+        public String uuid;
+        public double lat;
+        public double lng;
+
+        public WaterPin(Map<String,AttributeValue> map){
+
+        }
+}
