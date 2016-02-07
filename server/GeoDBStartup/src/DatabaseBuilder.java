195576cd4204fac73d82c0cc50f27cd3fdd8843c
Robert <robert.craynon@gmail.com>
2016-02-07 01:32:39 -0500

    Added db Builder- refactored some names

--- server/GeoDBStartup/src/DatabaseBuilder.java
+++ server/GeoDBStartup/src/DatabaseBuilder.java
@@ -58,7 +58,7 @@ import com.amazonaws.services.dynamodbv2.util.Tables;
  * This sample demonstrates how to perform a few simple operations with the
  * Amazon DynamoDB service.
  */
-public class AmazonDynamoDBSample {
+public class DatabaseBuilder {
 	private static GeoDataManagerConfiguration config;
 	private static GeoDataManager geoDataManager;
 	/*
@@ -75,6 +75,7 @@ public class AmazonDynamoDBSample {
 	static AmazonDynamoDBClient dynamoDB;
 	private static String accessKey = "AKIAIK3X7CV2F2OP7MYQ";
 	private static String secretKey = "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";
+	 
 
 	/**
 	 * The only information needed to create a client are security credentials
@@ -110,7 +111,7 @@ public class AmazonDynamoDBSample {
 		init();
 
 		try {
-			String tableName = "water-safe-locations-table";
+			String tableName = "water-safe-location-table";
 			config = new GeoDataManagerConfiguration(dynamoDB, tableName);
 			geoDataManager = new GeoDataManager(config);
 			// Create table if it does not exist yet
@@ -126,7 +127,7 @@ public class AmazonDynamoDBSample {
 				TableUtils.waitUntilActive(dynamoDB, tableName);
 			} catch (AmazonClientException e) {
 				System.out.println("Table " + tableName + " is already ACTIVE");
-
+				System.out.println(e);
 			}
 
 			// Describe our new table
@@ -171,9 +172,9 @@ public class AmazonDynamoDBSample {
 	private static void getEPAData(String url1, String url2) throws Exception {
 		// TODO take data from epa api and read into json
 
-		for (int i = 0; i < 2700000; i += 50){
+		for (int i = 0; i < 2700000; i += 100){
 			System.out.println("Test");
-			String sapiURL = url1 + Integer.toString(i) + ":" + Integer.toString(i +49) + url2;
+			String sapiURL = url1 + Integer.toString(i) + ":" + Integer.toString(i +99) + url2;
 			URL apiURL = new URL(sapiURL);
 			URLConnection con = apiURL.openConnection();
 			try {
